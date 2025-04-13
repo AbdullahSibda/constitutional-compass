6072e1787b68eff7d36c0827969e3951afa01176
abdullah <abdullahsibda29@gmail.com>
2025-04-13 21:55:48 +0200

    Pushing test to auth

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Login.css";
 import { useAuth } from "../../contexts/AuthContext";
-import { useNavigate } from "react-router-dom"; // Importing useNavigate for navigation
 
 const Login = () => {
   const { signIn } = useAuth();
@@ -21,45 +20,6 @@ const Login = () => {
       </section>
     </main>
   );
-};
-
-const styles = {
-  container: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "100vh",
-    backgroundColor: "#f5f5f5",
-  },
-  formContainer: {
-    padding: "20px",
-    borderRadius: "8px",
-    backgroundColor: "#fff",
-    boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
-    textAlign: "center",
-  },
-  title: {
-    marginBottom: "20px",
-    fontSize: "24px",
-    color: "#333",
-  },
-  googleButton: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: "10px 20px",
-    border: "none",
-    borderRadius: "4px",
-    backgroundColor: "#4285F4",
-    color: "#fff",
-    fontSize: "16px",
-    cursor: "pointer",
-  },
-  googleLogo: {
-    width: "20px",
-    height: "20px",
-    marginRight: "10px",
-  },
 };
 
 export default Login;
