c07add15807324cdfdd4a5a061660c721994271c
mamabolojeffrey <mamabolojeffrey99@gmail.com>
2025-04-13 15:40:50 +0200

    login_no divs

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -7,8 +7,8 @@ const Login = () => {
   const { signIn } = useAuth();
 
   return (
-    <div className="login-container">
-      <div className="login-form-container">
+    <main className="login-container">
+      <section className="login-form-container">
         <h2 className="login-title">Login</h2>
         <button className="login-google-button" onClick={signIn}>
           <img
@@ -18,8 +18,8 @@ const Login = () => {
           />
           Sign in with Google
         </button>
-      </div>
-    </div>
+      </section>
+    </main>
   );
 };
 
