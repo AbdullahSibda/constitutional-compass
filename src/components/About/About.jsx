3e33a7157c81c0a385f38d79ad459ec8346ff327
abdullah <abdullahsibda29@gmail.com>
2025-04-22 08:21:37 +0200

    fixed responsiveness

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
 import "./About.css";
-import { useState } from "react";
+import { useState, React } from "react";
 import Sidebar from "../Sidebar/Sidebar";
 
 function About() {
--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
 import "./Features.css";
-import { useState } from "react";
+import { useState, React } from "react";
 import Sidebar from "../Sidebar/Sidebar";
 
 function Features() {
